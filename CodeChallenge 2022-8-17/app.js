94d858c27e95387af5f55bca38704c95ee4ead53
Charon <charongaskins@gmail.com>
2022-08-18 14:31:50 -0400

    changes to code

--- CodeChallenge 2022-8-17/app.js
+++ CodeChallenge 2022-8-17/app.js
@@ -17,7 +17,7 @@ You can assume that you do get an array and if the array is empty, return 0.
 What We're Testing
 We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
 Advanced users may find this extremely easy and can easily write this in one line.
-
+ 
 */
 
 // Sum Numbers
